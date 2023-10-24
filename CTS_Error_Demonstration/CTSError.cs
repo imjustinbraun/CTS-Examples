7783c1febe163b5d0d653308f6325798e7cf0de1
Justin Braun <imjustinbraun@gmail.com>
2023-10-24 17:53:43 -0500

    initial commit

---
+++ CTS_Error_Demonstration/CTSError.cs
@@ -0,0 +1,22 @@
+﻿using T4;
+using T4.API;
+
+namespace CTS_Error_Demonstration
+{
+    public class CTSError
+    {
+        public Host CTSHost { get; set; }
+
+        public CTSError()
+        {
+            CTSHost = new(
+                APIServerType.Simulator,
+                "T4Example",
+                "112A04B0-5AAF-42F4-994E-FA7CB959C60B",
+                "CTSDev",
+                "testUser",
+                "123467"
+                );
+        }
+    }
+}
