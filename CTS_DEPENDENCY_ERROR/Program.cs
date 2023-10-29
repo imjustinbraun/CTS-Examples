b7c1de70c78bf2487f5170f1ed91db390237121c
Justin Braun <imjustinbraun@gmail.com>
2023-10-29 19:22:43 -0400

    updated to new error

--- CTS_DEPENDENCY_ERROR/Program.cs
+++ CTS_DEPENDENCY_ERROR/Program.cs
@@ -1,15 +1,18 @@
-﻿using T4;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using T4;
 using T4.API;
 
 namespace CTS_Error_Demonstration
 {
-    public class CTSError
+    public static class Program
     {
-        public Host CTSHost { get; set; }
-
-        public CTSError()
+        public static void Main(string[] args)
         {
-            CTSHost = new(
+            Host host = new(
                 APIServerType.Simulator,
                 "T4Example",
                 "112A04B0-5AAF-42F4-994E-FA7CB959C60B",
